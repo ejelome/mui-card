f532fd532cdf3f08df9f3077a7f4965d10cebfeb
ejelome <ejelome@gmail.com>
2020-09-07 23:00:42 +0200

    Change default render to Material-UI component

--- src/App.js
+++ src/App.js
@@ -7,25 +7,8 @@ import "fontsource-roboto/400.css";
 import "fontsource-roboto/500.css";
 import "fontsource-roboto/700.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Button } from "@material-ui/core";
+
+const App = () => <Button variant="contained">hello, world</Button>;
 
 export default App;
