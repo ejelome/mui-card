8e878e288177daf5b6d5f231f5f3e92997d31350
ejelome <ejelome@gmail.com>
2020-10-30 21:32:00 +0200

    Refactor src/App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// import logo from "./logo.svg";
-import "./App.css";
 import "fontsource-roboto/300.css";
 import "fontsource-roboto/400.css";
 import "fontsource-roboto/500.css";
@@ -17,6 +15,7 @@ import {
   Hidden,
   IconButton,
   Link,
+  Paper,
   Typography,
 } from "@material-ui/core";
 import { grey } from "@material-ui/core/colors";
@@ -25,16 +24,18 @@ import {
   makeStyles,
   responsiveFontSizes,
   ThemeProvider,
+  useTheme,
 } from "@material-ui/core/styles";
 import { Facebook, MoreHoriz } from "@material-ui/icons";
 import React from "react";
 
-const useStyles = makeStyles((theme) => ({
+let theme = createMuiTheme({ palette: { type: "dark" } });
+theme = responsiveFontSizes(theme);
+const useStyles = makeStyles(({ palette }) => ({
   root: {
     backgroundColor: grey[900],
-    maxWidth: theme.breakpoints.values.sm,
-    textAlign: "center",
-    "& h1": { color: grey[50] },
+    textAlign: "center", // near-black: #212121
+    "& h1": { color: grey[50] }, // near-white: #fafafa,
     "& .MuiCardHeader-root": { display: "inline" },
     "& .MuiCardHeader-avatar": { marginTop: theme.spacing(4) },
     "& .MuiCardActions-root": { display: "flex", justifyContent: "center" },
@@ -46,22 +47,21 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-let theme = createMuiTheme({ palette: { type: "dark" } });
-theme = responsiveFontSizes(theme);
-
 const App = () => {
   const { root } = useStyles();
+  const { palette } = useTheme();
+
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
       <Container className={root} disableGutters={true}>
-        <Card>
+        <CssBaseline />
+        <Card component={Paper}>
           <CardHeader
             avatar={<Avatar alt="" src="https://via.placeholder.com/96x96" />}
           />
           <CardContent>
             <Link variant="h1" color="inherit">
-              title
+              hello, {palette.type} title
             </Link>
             <Typography variant="subtitle1">subtitle</Typography>
             <Divider variant="middle" />
